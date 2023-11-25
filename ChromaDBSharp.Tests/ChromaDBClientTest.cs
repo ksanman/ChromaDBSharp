06fffbe0f7b45b227b269b5470e0f01ff23fc558
ksanman <agent.xaber@gmail.com>
2023-11-24 23:26:25 -0700

    more tests

--- ChromaDBSharp.Tests/ChromaDBClientTest.cs
+++ ChromaDBSharp.Tests/ChromaDBClientTest.cs
@@ -52,5 +52,31 @@ namespace ChromaDBSharp.Tests
             ICollectionClient collection = client.CreateCollection("test", createOrGet: true);
             Assert.NotNull(collection);
         }
+
+        [Fact]
+        public void GetCollection()
+        {
+            ChromaDBClient client = new(_httpClient);
+            ICollectionClient collection = client.GetCollection("test");
+            Assert.NotNull(collection);
+        }
+
+        [Fact]
+        public void UpdateCollection()
+        {
+            ChromaDBClient client = new(_httpClient);
+            //TODO - need a better way for metadata, JSON does serialize correctly...
+            client.UpdateCollection("test", "test", new Dictionary<string, object>
+            {
+                {"id1", "value1" }
+            });
+        }
+
+        [Fact]
+        public void DeleteCollection()
+        {
+            ChromaDBClient client = new(_httpClient);
+            client.DeleteCollection("test");
+        }
     }
 }
--- ChromaDBSharp.Tests/TestFixture.cs
+++ ChromaDBSharp.Tests/TestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using ChromaDBSharp.Models;
 using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace ChromaDBSharp.Tests
 {
@@ -14,7 +15,9 @@ namespace ChromaDBSharp.Tests
             {
                 { "/api/v1/version", Version },
                 { "/api/v1/heartbeat", Heartbeat },
-                { "/api/v1/collections", Collections }
+                { "/api/v1/collections", Collections },
+                { "/api/v1/reset", Reset },
+                { "/api/v1/collections/test", CollectionName }
             }))
             {
                 BaseAddress = new Uri("http://localhost:8000/")
@@ -51,12 +54,23 @@ namespace ChromaDBSharp.Tests
             return response;
         }
 
+        private static HttpResponseMessage Reset(HttpRequestMessage message)
+        {
+
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = JsonContent.Create(true)
+            };
+            return response;
+        }
+
         private static HttpResponseMessage Collections(HttpRequestMessage message)
         {
             return message.Method.Method switch
             {
                 "GET" => ListCollections(message),
                 "POST" => CreateCollection(message),
+                "DELETE" => DeleteCollection(message),
                 _ => throw new NotImplementedException(),
             };
         }
@@ -107,6 +121,63 @@ namespace ChromaDBSharp.Tests
                 {
                     { "TestMD", "value" }
                 }
+            };
+
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = JsonContent.Create(responseObject)
+            };
+            return response;
+        }
+
+        private static HttpResponseMessage CollectionName(HttpRequestMessage message)
+        {
+            switch (message.Method.Method)
+            {
+                case "DELETE": return DeleteCollection(message);
+                case "GET": return GetCollection(message);
+                case "PUT": return UpdateCollection(message);
+                default: throw new NotImplementedException();
+            }
+        }
+
+        private static HttpResponseMessage DeleteCollection(HttpRequestMessage message)
+        {
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+        }
+
+        private static HttpResponseMessage GetCollection(HttpRequestMessage message)
+        {
+            var responseObject = new Collection
+            {
+                Id = "c4c1a65f-db2a-4372-a0b4-8e1f03ab6985",
+                Database = "default_database",
+                Tenant = "default_tenant",
+                Name = "Test",
+                Metadata = new Dictionary<string, string>
+                {
+                    { "TestMD", "value" }
+                }
+            };
+
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = JsonContent.Create(responseObject)
+            };
+            return response;
+        }
+
+        public static HttpResponseMessage UpdateCollection(HttpRequestMessage message)
+        {
+            var json = message.Content.ReadAsStringAsync().Result;
+            var content = JsonSerializer.Deserialize<UpdateCollectionRequest>(json); 
+            var responseObject = new Collection
+            {
+                Id = "c4c1a65f-db2a-4372-a0b4-8e1f03ab6985",
+                Database = "default_database",
+                Tenant = "default_tenant",
+                Name = content.NewName,
+                Metadata = content.NewMetadata.Select(kv => new KeyValuePair<string, string>(kv.Key, kv.Value.ToString())).ToDictionary()
             };
 
             var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
