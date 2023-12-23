b4d4b41b2559843f9c5af586187995703c9e628b
ksanman <agent.xaber@gmail.com>
2023-12-23 01:18:40 -0700

    Add query result

--- ChromaDBSharp/Models/QueryResult.cs
+++ ChromaDBSharp/Models/QueryResult.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace ChromaDBSharp.Models
 {
     public class QueryResult
     {
+        [JsonProperty("ids")]
+        public IEnumerable<IEnumerable<string>>? Ids { get; set; } = null;
+        [JsonProperty("distances")]
+        public IEnumerable<IEnumerable<float>>? Distances { get; set; } = null;
+        [JsonProperty("metadatas")]
+        public IEnumerable<IEnumerable<IDictionary<string, string>?>> Metadatas { get; set; } = Enumerable.Empty<IEnumerable<IDictionary<string, string>?>>();
+        [JsonProperty("embeddings")]
+        public IEnumerable<IEnumerable<float>>? Embeddings { get; set; } = null;
+        [JsonProperty("documents")] 
+        public IEnumerable<IEnumerable<string?>> Documents { get; set; } = Enumerable.Empty<IEnumerable<string?>>();
+        [JsonProperty("uris")] 
+        public IEnumerable<IEnumerable<string>>? Uris { get; set; } = null;
+        [JsonProperty("data")] 
+        public IEnumerable<IEnumerable<string>>? Data { get; set; } = null;
     }
 }
